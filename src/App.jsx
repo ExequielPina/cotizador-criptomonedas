e25ee9a7363e7f11ce68846c7bfa00ac9f045d34
ExequielPina <exequiel_pina@hotmail.es>
2022-12-12 18:09:16 +0100

    comit 1.1 creando el formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
 import { useState } from 'react'
 import styled from '@emotion/styled'
+import ImagenCripto from './img/imagen-criptos.png'
+import Formulario from './components/Formulario'
 
 
 const Headin = styled.h1`
   font-family: 'Lato', sans-serif;
   color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
+
+const Contenedor = styled.div`
+ max-width: 900px;
+ margin: 0 auto;
+ width: 90%;
+ @media (min-width: 992px) {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 2rem;
+ }
 `
  
 
@@ -12,7 +45,17 @@ function App() {
   
 
   return (
-    <Headin>Desde appa</Headin>
+    <Contenedor>
+      <Imagen src={ImagenCripto} alt="imagenes-criptomonedas"></Imagen>
+      <div>
+        <Headin>Cotizador de criptomonedas al instante</Headin>
+        
+        <Formulario/>
+      </div>
+      
+      
+    </Contenedor>
+    
   )
 }
 
