57d95b63446ca379919d0b20f2684ae4085f082d
ExequielPina <exequiel_pina@hotmail.es>
2022-12-12 13:57:35 +0100

    comit 1.0 creación del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import styled from '@emotion/styled'
+
+
+const Headin = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #fff;
+`
+ 
+
+function App() {
+  
+
+  return (
+    <Headin>Desde appa</Headin>
+  )
+}
+
+export default App
+
