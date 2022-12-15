c844506b6995501a7a5d822965cf0b0ea91c9618
ExequielPina <exequiel_pina@hotmail.es>
2022-12-15 12:56:53 +0100

    comit 1.3 consultando la API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
 import ImagenCripto from './img/imagen-criptos.png'
 import Formulario from './components/Formulario'
@@ -43,6 +43,27 @@ const Contenedor = styled.div`
  
 
 function App() {
+
+  const [ monedas, setMonedas ] = useState({})
+  const [ resultado, setResultado ] = useState({})
+
+  useEffect(() => {
+    if(Object.keys(monedas).length > 0) {
+
+      const cotizarCripto = async () => {
+        const { moneda, criptomoneda} = monedas
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
+        
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+        setResultado(resultado.DISPLAY[criptomoneda] [moneda])
+      }
+      
+      cotizarCripto()
+    }
+    
+
+  }, [monedas])
   
 
   return (
@@ -51,7 +72,9 @@ function App() {
       <div>
         <Headin>Cotizador de criptomonedas al instante</Headin>
 
-        <Formulario/>
+        <Formulario 
+          setMonedas={setMonedas}
+        />
       </div>
       
       
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -24,7 +24,7 @@ const InputSubmit = styled.input`
 `
 
 
-const Formulario = () => {
+const Formulario = ({setMonedas}) => {
 
   const [ criptos, setCriptos ] = useState([])
   const [ error, setError ] = useState(false)
@@ -63,7 +63,11 @@ const Formulario = () => {
 
           return
         }
+
         setError(false)
+        setMonedas({
+          moneda, criptomoneda
+        })
       }
   
 
